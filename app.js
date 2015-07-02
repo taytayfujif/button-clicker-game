8d3617ee6b72c3d3fb1d604281c440d88a3287c1
Charless Nguyen <cnguyen@ssac02kx8e8f5n7.punahou.edu>
2015-07-02 11:44:17 -1000

    test

---
+++ app.js
@@ -0,0 +1,12 @@
+var Uscore = 0;
+
+var highscore = 10;
+
+var press = function() {
+	Uscore++;
+	document.getElementById('score').innerHTML = "Your Score: " + Uscore;
+	if (Uscore > highscore) {
+		document.getElementById('hiscore').innerHTML = "New Highscore: " + Uscore;
+		return;
+	}
+};
