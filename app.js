51563d1e18a7579a33443fa9e9d42e2e10569d0a
Charles6 <charles@devleague.com>
2015-09-30 13:53:20 -1000

    Bonus
    Added bonus button

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ var highscore = 10;
 var press = function() {
 	Uscore++;
 	document.getElementById('score').innerHTML = "Your Score: " + Uscore;
+
+	if (Uscore === 20) {
+		alert("Bonus Button Unlocked!");
+		document.getElementById('bonus').innerHTML = 
+		'<input onclick="bonuspress()" type="image" id="button" src="button.jpg">';
+	}
+
+
 	if (Uscore > highscore) {
 		document.getElementById('hiscore').innerHTML = "New Highscore: " + Uscore;
 		return;
 	}
 };
+
+var bonuspress = function() {
+	Uscore = Uscore + 9;
+	document.getElementById('score').innerHTML = "Your Score: " + Uscore;
+}
