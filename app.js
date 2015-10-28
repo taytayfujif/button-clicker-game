44a656dfdf3a299d5d244ae74b6ab45f557a5e53
Charles6 <charles@devleague.com>
2015-10-28 13:15:41 -1000

    Simple
    Simplified the app for the teacher training workshop

--- app.js
+++ app.js
@@ -6,29 +6,8 @@ var press = function() {
 	Uscore++;
 	document.getElementById('score').innerHTML = "Your Score: " + Uscore;
 
-	if (Uscore === 20) {
-		alert("Bonus Button Unlocked!");
-		timerbutton();
-		setTimeout(removeButton, 4000);
-	}
-
-
 	if (Uscore > highscore) {
 		document.getElementById('hiscore').innerHTML = "New Highscore: " + Uscore;
 		return;
 	}
 };
-
-var timerbutton = function() {
-	document.getElementById('bonus').innerHTML = 
-		'<input onclick="bonuspress()" type="image" id="button" src="button.jpg">';
-}
-
-var removeButton = function() {
-	document.getElementById('bonus').innerHTML = "";
-}
-
-var bonuspress = function() {
-	Uscore = Uscore + 10;
-	document.getElementById('score').innerHTML = "Your Score: " + Uscore;
-}
