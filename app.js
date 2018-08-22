c4c6933e252556d73b11d2381eba8bd29b8cfb08
Taylor <tyafujimoto@gmail.com>
2018-08-22 13:34:33 -1000

    final commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var score= 0;
+document.getElementById("button").addEventListener("click", function(){
+  score=   score +1
+document.getElementById("button").innerHTML = score
+    console.log(score);
+});
+
+
+
